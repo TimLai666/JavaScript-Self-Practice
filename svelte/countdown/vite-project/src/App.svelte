d99cfc7a7cf746e8e86957b2bd95777400d9b9fe
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-11-01 22:53:05 +0800

    update

---
+++ svelte/countdown/vite-project/src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import {afterUpdate, onDestroy, onMount} from 'svelte'
+
+  let countDown: number = 10
+  let timer: number
+
+  onMount(() => {
+    timer = setInterval(() => {
+      countDown--
+    }, 1000)
+  })
+
+  afterUpdate(() => {
+    if (countDown === 0) {
+      if (timer) {
+        clearInterval(timer)
+        timer = 0
+      }
+    }
+  })
+
+  onDestroy(() => {
+    if (timer) {
+      clearInterval(timer)
+    }
+  })
+</script>
+
+<h1>{countDown}</h1>
