133022f152691f20a9829d732d2544cc987b0399
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-11-01 23:23:17 +0800

    update

--- svelte/countdown/src/App.svelte
+++ svelte/countdown/src/App.svelte
@@ -1,29 +1,5 @@
 <script lang="ts">
-  import {afterUpdate, onDestroy, onMount} from 'svelte'
-
-  let countDown: number = 10
-  let timer: number
-
-  onMount(() => {
-    timer = setInterval(() => {
-      countDown--
-    }, 1000)
-  })
-
-  afterUpdate(() => {
-    if (countDown === 0) {
-      if (timer) {
-        clearInterval(timer)
-        timer = 0
-      }
-    }
-  })
-
-  onDestroy(() => {
-    if (timer) {
-      clearInterval(timer)
-    }
-  })
+  import Countdown from "./Countdown.svelte"
 </script>
 
-<h1>{countDown}</h1>
+<Countdown countDown={20} />
---
+++ svelte/countdown/src/Countdown.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import {afterUpdate, onDestroy, onMount} from 'svelte'
+  
+    // 預設是10
+    export let countDown: number = 10
+    let timer: number
+  
+    // 00:xx
+    $: displayValue = `00:${countDown.toString().padStart(2, '0')}`
+  
+    onMount(() => {
+      timer = setInterval(() => {
+        countDown--
+      }, 1000)
+    })
+  
+    afterUpdate(() => {
+      if (countDown === 0) {
+        if (timer) {
+          clearInterval(timer)
+          timer = 0
+        }
+      }
+    })
+  
+    onDestroy(() => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    })
+  </script>
+  
+  <h1>{displayValue}</h1>
+  
+  {#if countDown === 0}
+    <h2>時間到</h2>
+  {/if}
