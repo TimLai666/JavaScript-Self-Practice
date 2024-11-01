5dc9b387dd4238ba2ac847db16e4444c19fd990e
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-11-01 23:27:12 +0800

    Update Countdown.svelte

--- svelte/countdown/src/Countdown.svelte
+++ svelte/countdown/src/Countdown.svelte
@@ -1,37 +1,43 @@
 <script lang="ts">
-    import {afterUpdate, onDestroy, onMount} from 'svelte'
-  
-    // 預設是10
-    export let countDown: number = 10
-    let timer: number
-  
-    // 00:xx
-    $: displayValue = `00:${countDown.toString().padStart(2, '0')}`
-  
-    onMount(() => {
-      timer = setInterval(() => {
-        countDown--
-      }, 1000)
-    })
-  
-    afterUpdate(() => {
-      if (countDown === 0) {
-        if (timer) {
-          clearInterval(timer)
-          timer = 0
-        }
-      }
-    })
-  
-    onDestroy(() => {
+  import { afterUpdate, onDestroy, onMount } from "svelte";
+
+  // 預設是10
+  export let countDown: number = 10;
+  let timer: number;
+  const arr = [1, 2, 3, 4];
+
+  // 00:xx
+  $: displayValue = `00:${countDown.toString().padStart(2, "0")}`;
+
+  onMount(() => {
+    timer = setInterval(() => {
+      countDown--;
+    }, 1000);
+  });
+
+  afterUpdate(() => {
+    if (countDown === 0) {
       if (timer) {
-        clearInterval(timer)
+        clearInterval(timer);
+        timer = 0;
       }
-    })
-  </script>
-  
-  <h1>{displayValue}</h1>
-  
-  {#if countDown === 0}
-    <h2>時間到</h2>
-  {/if}
+    }
+  });
+
+  onDestroy(() => {
+    if (timer) {
+      clearInterval(timer);
+    }
+  });
+</script>
+
+<h1>{displayValue}</h1>
+
+{#each arr as num }
+  <span>{num}</span>
+  <br/>
+{/each}
+
+{#if countDown === 0}
+  <h2>時間到</h2>
+{/if}
