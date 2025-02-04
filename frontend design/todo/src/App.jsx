cbf343066ed94f310421980451cb47230c58dbf8
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-02-04 12:50:18 +0800

    renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/.gitignore" -> frontend design/todo/.gitignore 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/README.md" -> frontend design/todo/README.md 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/eslint.config.js" -> frontend design/todo/eslint.config.js 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/index.html" -> frontend design/todo/index.html 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/package-lock.json" -> frontend design/todo/package-lock.json 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/package.json" -> frontend design/todo/package.json 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/public/vite.svg" -> frontend design/todo/public/vite.svg 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/src/App.css" -> frontend design/todo/src/App.css 	new file:   frontend design/todo/src/App.jsx 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/src/assets/react.svg" -> frontend design/todo/src/assets/react.svg 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/src/index.css" -> frontend design/todo/src/index.css 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/src/main.jsx" -> frontend design/todo/src/main.jsx 	renamed:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/vite.config.js" -> frontend design/todo/vite.config.js 	deleted:    "\345\211\215\347\253\257\347\266\262\347\253\231\350\250\255\350\250\210\345\257\246\346\210\260/todo/src/App.jsx"

---
+++ frontend design/todo/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [todoItems, setTodoItems] = useState([]);
+  const createTodoItem = () => {
+    setTodoItems([name, ...todoItems]);
+  };
+
+  return (
+    <>
+      <h1 className="title">Todo</h1>
+      <hr />
+      <div className="input-form">
+        <input
+          type="text"
+          placeholder="待辦事項"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={createTodoItem}>新增</button>
+      </div>
+      {todoItems.map((item) => {
+        return <div className="todo-item">{item}</div>;
+      })}
+    </>
+  );
+}
+
+export default App;
